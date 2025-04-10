2fb71b58c8bec0100f073f1aa35039acc6348292
Edward Lee <7edwardlee@gmail.com>
2025-04-09 19:27:40 -0700

    completed all test cases

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,86 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        let sum = lhs + rhs
+        return sum
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        let difference = lhs - rhs
+        return difference
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        let product = lhs * rhs
+        return product
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        let quotient = lhs / rhs
+        return quotient
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ nums: [Int]) -> Int {
+        var sum = 0
+        for num in nums {
+            sum += num
+        }
+        return sum
+    }
+    
+    func multiply(_ nums: [Int]) -> Int {
+        var product = 0
+        for num in nums {
+            product += num
+        }
+        return product
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+    
+    func avg (_ args: [Int]) -> Int {
+        var sum = 0
+        for num in args {
+            sum += num
+        }
+        return sum / args.count
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var result = beg
+        for num in args {
+            result = op(result, num)
+        }
+        return result
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+            return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+        }
+
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+            return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+        }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+            let x = lhs["x"]! + rhs["x"]!
+            let y = lhs["y"]! + rhs["y"]!
+            return ["x": x, "y": y]
+        }
+
+        func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+            let x = lhs["x"]! - rhs["x"]!
+            let y = lhs["y"]! - rhs["y"]!
+            return ["x": x, "y": y]
+        }
+
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -43,6 +123,12 @@ let calc = Calculator()
 //: Keep in mind that writing new tests may reveal ambiguity in the specification above--if that's the case, document the ambiguity, declare what you think *should* happen, and write the test to test for it.
 
 // ===== Your tests go here
+calc.add(lhs: -4, rhs: -10) == -14
+calc.subtract(lhs: -10, rhs: -5) == -5
+calc.subtract(lhs: 10, rhs: -5) == 15
+calc.multiply(lhs: -4, rhs: -3) == 12
+calc.multiply(lhs: 10, rhs: -3) == -30
+calc.divide(lhs: -8, rhs: 4) == -2
 
 //: ---
 //: ## Test code block
@@ -52,7 +138,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
@@ -68,10 +154,10 @@ calc.avg([1]) == 1
 calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
     // this is (((0 op 1) op 2) op 3)
 calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
-    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+//    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
 calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
-    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-
+//    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+//
 let p1 = (5, 5)
 let p2 = (12, -27)
 let p3 = (-4, 4)
